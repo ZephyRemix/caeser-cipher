a0c8cf841f222ae8b4897813d8db5c04b70497bf
ZephyRemix <hunyaochong@gmail.com>
2024-07-10 08:32:23 +0800

    Add caeser cipher encryption function

---
+++ cipher.rb
@@ -0,0 +1,18 @@
+def caesar_cipher(str, n)
+    letters = ('a'..'z').to_a
+    n = n % letters.size  # Normalize the shift value
+
+    str.chars.map { |c| shift_char(c, n, letters) }.join
+end
+
+def shift_char(c, n, letters)
+    index = letters.find_index(c.downcase)
+    return c unless index
+
+    shifted_char = letters[(index + n) % letters.size]
+    
+    c == c.upcase ? shifted_char.upcase : shifted_char
+end
+
+str = gets.chomp
+p caesar_cipher(str, 5)
