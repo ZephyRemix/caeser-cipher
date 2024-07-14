1083bca290b715818c5f7e079971c3a3605dc15f
ZephyRemix <hunyaochong@gmail.com>
2024-07-14 15:40:49 +0800

    Add rubocop functionality

--- cipher.rb
+++ cipher.rb
@@ -1,17 +1,19 @@
-def caesar_cipher(str, n)
-    letters = ('a'..'z').to_a
-    n = n % letters.size  # Normalize the shift value
+require 'rubocop'
 
-    str.chars.map { |c| shift_char(c, n, letters) }.join
+def caesar_cipher(str, n)
+  letters = ('a'..'z').to_a
+  n %= letters.size # Normalize the shift value
+
+  str.chars.map { |c| shift_char(c, n, letters) }.join
 end
 
 def shift_char(c, n, letters)
-    index = letters.find_index(c.downcase)
-    return c unless index
+  index = letters.find_index(c.downcase)
+  return c unless index
 
-    shifted_char = letters[(index + n) % letters.size]
-    
-    c == c.upcase ? shifted_char.upcase : shifted_char
+  shifted_char = letters[(index + n) % letters.size]
+
+  c == c.upcase ? shifted_char.upcase : shifted_char
 end
 
 str = gets.chomp
