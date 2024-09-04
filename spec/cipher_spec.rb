865279dafb65b3765a613466071a106494510428
ZephyRemix <hunyaochong@gmail.com>
2024-09-04 12:23:38 +0800

    Added unit test for edge cases on encrypting messages

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -3,8 +3,10 @@ require_relative '../cipher.rb'
 describe Cipher do
 
   describe '#normalize_shift_factor' do
+    subject(:cipher) { described_class.new('test', shift_factor) }
+
     context "when given a shift factor lower than length of letter array" do
-      subject(:cipher) { described_class.new('test', 1) }
+      let(:shift_factor) { 1 }
 
       it 'returns the same shift value' do
         expect{ cipher.normalize_shift_factor}.to_not change { cipher.instance_variable_get(:@shift_factor) }
@@ -12,22 +14,38 @@ describe Cipher do
     end
 
     context "when given a shift factor larger than length of letter array" do
-      subject(:cipher) { described_class.new('test', 30) }
+      let(:shift_factor) { 30 }
 
       it 'returns a normalized value' do
-        normalized_val = 30 - cipher.instance_variable_get(:@cipher_dict).size
-        expect{ cipher.normalize_shift_factor }.to change { cipher.instance_variable_get(:@shift_factor) }.from(30).to(normalized_val)
+        expect{ cipher.normalize_shift_factor }.to change { cipher.instance_variable_get(:@shift_factor) }.from(30).to(4)
       end
     end
   end
 
   describe '#encrypt_message' do
     context 'when encrpyting a string with letters of upper and lower casings, and non-alphabets' do
-      subject(:cipher_encrypt) { described_class.new('"What a string!123"', 5) }
+      subject(:cipher) { described_class.new('"What a string!123"', 5) }
 
       it 'returns a string preserving casing and non-alphabets' do
-        expect(cipher_encrypt.encrypt_message).to eql('"Bmfy f xywnsl!123"')
+        expect(cipher.encrypt_message).to eql('"Bmfy f xywnsl!123"')
+      end
+    end
+
+    context 'when encrpyting an empty string' do
+      subject(:cipher) { described_class.new('', 5) }
+
+      it 'returns an empty string' do
+        expect(cipher.encrypt_message).to eql('')
+      end
+    end
+
+    context 'when encrpyting with shift factor of 0' do
+      subject(:cipher) { described_class.new('What a string!123', 0) }
+
+      it 'returns the original string' do
+        expect(cipher.encrypt_message).to eql('What a string!123')
       end
     end
   end
 end
+
